c1f155be9e810181b8e8264eac67fa608478002a
Travis CI <travis@travis-ci.org>
2023-11-10 18:52:08 -0500

    changin name

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/m/Documents/web/assytemplate/app/layout.tsx": [
+      "/home/m/Documents/web/assytemplate/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+      "/home/m/Documents/web/assytemplate/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/CalSans-SemiBold.ttf\",\"variable\":\"--font-calsans\"}],\"variableName\":\"calSans\"}",
+      "/home/m/Documents/web/assytemplate/global.css"
+    ]
+  },
+  "cssModules": {
+    "/home/m/Documents/web/assytemplate/app/page": [
+      "/home/m/Documents/web/assytemplate/global.css",
+      "/home/m/Documents/web/assytemplate/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+      "/home/m/Documents/web/assytemplate/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/CalSans-SemiBold.ttf\",\"variable\":\"--font-calsans\"}],\"variableName\":\"calSans\"}"
+    ]
+  }
+}
